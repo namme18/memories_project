9819b514871175694989e18fa3fed54a4e41fc2e
namme18 <nammeselarom@gmail.com>
2021-06-19 14:33:39 +0800

    push to heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -24,6 +25,16 @@ mongoose
         useFindAndModify: false})
     .then(res => console.log('MongoDb connected!'))
     .catch(err => console.log(err.message));
+
+//serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
